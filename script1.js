1c9c8f5cad41f52c4e2649806fd41dce4e17078b
thuthuin9601 <96036874+thuthuin9601@users.noreply.github.com>
2022-02-22 18:31:03 +0700

    done

--- script1.js
+++ script1.js
@@ -8,20 +8,20 @@ let enemyTimer = 0;
 
 const flyingDino = new Image();
 flyingDino.src = 'flying dino.png';
-const jumpingDino = new Image();
-jumpingDino.src = 'jumping dino.png';
+const jumpingSpider = new Image();
+jumpingSpider.src = 'spider.png';
 const runningDino = new Image();
 runningDino.src = 'running dino.png';
 
 let enemies = [];
-const enemyTypes = ['running'];
+const enemyTypes = ['running','flying','jumping'];
 
 class Enemy {
     constructor(){
         this.gameWidth = canvas.width;
         this.gameHeight = canvas.height;
         this.markedForDeletion = false;
-        this.frameX;
+        this.frameX=0;
        
         // this.fps = 20;
         this.frameInterval = 100;// có thể chia fps
@@ -42,7 +42,7 @@ class Enemy {
                 this.frameX = 0;
             }
         }
-        else if (this.frameTimer < this.frameInterval){
+        else{
             this.frameTimer += deltaTime;
         }
     }
@@ -69,13 +69,58 @@ class RunningDino extends Enemy {
         super.update(deltaTime);
     }
 };
-
+class FlyingDino extends Enemy {
+    constructor(){
+        super();
+        this.maxFrame = 6; //tí phải đổi
+        this.spriteWidth = 1200/7;
+        this.spriteHeight = 151;
+        this.x = this.gameWidth;
+        this.y =this.gameHeight*Math.random() *0.6;
+        this.width = this.spriteWidth/2;
+        this.height = this.spriteHeight/2;
+        this.speed = Math.random()*0.1 + 0.1;
+        this.image = flyingDino;
+        this.angle = 0;
+        this.curve = Math.random() * 3;
+    }
+    update(deltaTime){
+        super.update(deltaTime);
+        this.y += Math.sin(this.angle)* this.curve;
+        this.angle += 0.1;
+    }
+};
+class JumpingSpider extends Enemy {
+    constructor(){
+        super();
+        this.maxFrame = 7; 
+        this.spriteWidth = 1254/8;
+        this.spriteHeight = 91;
+        this.x = this.gameWidth;
+        this.y =this.gameHeight*Math.random() *0.6;
+        this.width = this.spriteWidth;
+        this.height = this.spriteHeight;
+        this.speed = Math.random()*0.1 + 0.1;
+        this.image = jumpingSpider;
+        this.vy = 0;
+        this.weight = 1;
+    }
+    update(deltaTime){
+        super.update(deltaTime);
+        this.y += this.vy;
+        if(this.y < this.gameHeight - this.height){
+            this.vy += this.weight;// khi vy <0 thì nvat sẽ nhảy lên, đạt đỉnh khi vy = 0, vy >0 sẽ đi xuống
+        } else {
+            this.vy = -15;
+        }
+    }
+};
 function update(deltaTime){
     enemies =  enemies.filter(object => !object.markedForDeletion)   
     if(enemyTimer>enemyInterval){
         addNewEnemy();
         enemyTimer = 0;
-        console.log(enemies);
+        // console.log(enemies);
     }
     else {
         enemyTimer+=deltaTime;
@@ -86,7 +131,17 @@ function draw(){
     enemies.forEach(object => object.draw(ctx));
 }
 function addNewEnemy(){
-    enemies.push(new RunningDino());
+   
+    let randomEnemy = enemyTypes[Math.floor(Math.random()*enemyTypes.length)]
+    if (randomEnemy == 'running'){
+        enemies.push(new RunningDino());
+    }
+    else if (randomEnemy == 'flying'){
+        enemies.push(new FlyingDino());
+    }
+    else if (randomEnemy == 'jumping'){
+        enemies.push(new JumpingSpider());
+    }
 }
 
 let lastTime = 0;
