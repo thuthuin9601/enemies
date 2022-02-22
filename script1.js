dfd6c7c608bf0e1d164a14b58156c011aa0eddec
thuthuin9601 <96036874+thuthuin9601@users.noreply.github.com>
2022-02-22 11:03:53 +0700

    Update script1.js

--- script1.js
+++ script1.js
@@ -2,46 +2,89 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 500;
 const CANVAS_HEIGHT = canvas.height = 500;
-let enemyInterval = 100;
+let enemyInterval = 400;
 let enemyTimer = 0;
 
+
+const flyingDino = new Image();
+flyingDino.src = 'flying dino.png';
+const jumpingDino = new Image();
+jumpingDino.src = 'jumping dino.png';
+const runningDino = new Image();
+runningDino.src = 'running dino.png';
+
 let enemies = [];
-
-
+const enemyTypes = ['running'];
 
 class Enemy {
     constructor(){
         this.gameWidth = canvas.width;
         this.gameHeight = canvas.height;
-        this.x = this.gameWidth;
-        this.y = Math.random()*this.gameHeight
-        this.width = 100;
-        this.height = 100;
+        this.markedForDeletion = false;
+        this.frameX;
+        this.maxFrame = 7; //tí phải đổi
+        // this.fps = 20;
+        this.frameInterval = 100;// có thể chia fps
+        this.frameTimer = 0;
     }
-    update(){
-        this.x--;
+    update(deltaTime){
+        this.x -= this.speed*deltaTime; 
+        //remove enemies
+        if (this.x + this.width < 0){
+            this.markedForDeletion = true;
+        }
+        if (this.frameTimer > this.frameInterval){
+            if (this.frameX < this.maxFrame){
+                this.frameX++
+            }
+            else {
+                this.frameX = 0;
+            }
+        }
+        else if (this.frameTimer < this.frameInterval){
+            this.frameTimer += deltaTime;
+        }
     }
-    draw(){
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+    draw(ctx){
+        ctx.drawImage(this.image, this.frameX* this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
-function update(){
 
+class RunningDino extends Enemy {
+    constructor(){
+        super();
+        this.spriteWidth = 94.5;
+        this.spriteHeight = 72;
+        this.x = this.gameWidth;
+        this.y =this.gameHeight - this.spriteHeight;
+        this.width = this.spriteWidth;
+        this.height = this.spriteHeight;
+        this.speed = Math.random()*0.1 + 0.1;
+        this.image = runningDino;
+        // console.log(this.height);
+    }
+    update(deltaTime){
+        super.update(deltaTime);
+    }
+};
+
+function update(deltaTime){
+    enemies =  enemies.filter(object => !object.markedForDeletion)   
     if(enemyTimer>enemyInterval){
         addNewEnemy();
         enemyTimer = 0;
         console.log(enemies);
     }
     else {
-        enemyTimer++;
+        enemyTimer+=deltaTime;
     }
-    enemies.forEach(object => object.update());
+    enemies.forEach(object => object.update(deltaTime));
 }
 function draw(){
     enemies.forEach(object => object.draw(ctx));
 }
 function addNewEnemy(){
-    enemies.push(new Enemy());
+    enemies.push(new RunningDino());
 }
 
 let lastTime = 0;
@@ -49,7 +92,7 @@ function animate(timeStamp){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     let deltaTime = timeStamp - lastTime;
     lastTime = timeStamp;
-    update();
+    update(deltaTime);
     draw();
     requestAnimationFrame(animate);
 }
